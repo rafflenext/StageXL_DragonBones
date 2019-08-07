1127a78bb3b4268617815849da9dddf3310b09f1
SpivEgin <spivegin@txtsme.com>
2019-08-06 23:40:20 -0400

    working on coaTools

--- lib/src/dragon_bones.dart
+++ lib/src/dragon_bones.dart
@@ -1,35 +1,33 @@
 part of stagexl_dragonbones;
 
 class DragonBones {
-
   final String name;
   final String version;
   final int frameRate;
   final bool isGlobal;
   final List<Armature> armatures;
 
-  DragonBones(
-      this.name,
-      this.version,
-      this.frameRate,
-      this.isGlobal,
-      this.armatures);
+  DragonBones(this.name, this.version, this.frameRate, this.isGlobal, this.armatures);
 
   //---------------------------------------------------------------------------
 
   static DragonBones fromJson(String jsonSource) {
     var data = json.decode(jsonSource);
-    var version = data["version"];
-    if (version == "4.0") return _DragonBonesParserJson4.parse(data);
-    if (version == "4.5") return _DragonBonesParserJson4.parse(data);
-    if (version == "5.5") return _DragonBonesParserJson5.parse(data);
+    String ver = data["version"];
+    //todo add error handling
+    double version = double.parse(ver);
+    assert(version is double);
+    print(version);
+    if (version == 4.0) return _DragonBonesParserJson4.parse(data);
+    if (version == 4.5) return _DragonBonesParserJson4.parse(data);
+    if (version > 5 && version < 6) return _coaToolsParserJson.parse(data);
     throw new ArgumentError("Unsupported format.");
   }
 
   //--x-------------------------------------------------------------------------
 
   Armature getArmature(String armatureName) {
-    for(var armature in armatures) {
+    for (var armature in armatures) {
       if (armature.name == armatureName) {
         return armature;
       }
@@ -42,5 +40,4 @@ class DragonBones {
     if (armature == null) throw new ArgumentError("armatureName");
     return new Skeleton(armature, this.frameRate);
   }
-
 }
--- lib/src/dragon_bones_parser_json5.dart
+++ lib/src/dragon_bones_parser_json5.dart
@@ -1,9 +1,10 @@
 part of stagexl_dragonbones;
-
-class _DragonBonesParserJson5 {
-
+//_coaToolsParserJson Support for COA_Tools and Blender 2.8
+class _coaToolsParserJson {
+  
   // Format: http://edn.egret.com/cn/index.php/article/index/id/338
-
+  //  https://github.com/rafflenext/dragonBonesJsonFormat.git
+  
   static DragonBones parse(Map data) {
     return new DragonBones(
         _getString(data, "name", ""),
@@ -12,9 +13,9 @@ class _DragonBonesParserJson5 {
         _getBool(data, "isGlobal", false),
         _getList<Armature>(data, "armature", _parseArmature));
   }
-
+  
   //---------------------------------------------------------------------------
-
+  
   static Armature _parseArmature(Map data) {
     return new Armature(
         _getString(data, "name", ""),
@@ -23,7 +24,7 @@ class _DragonBonesParserJson5 {
         _getList<Skin>(data, "skin", _parseSkin),
         _getList<Animation>(data, "animation", _parseAnimation));
   }
-
+  
   static Bone _parseBone(Map data) {
     return new Bone(
         _getString(data, "name", ""),
@@ -31,7 +32,7 @@ class _DragonBonesParserJson5 {
         _getInt(data, "length", 0),
         _getTransform(data, "transform"));
   }
-
+  
   static Slot _parseSlot(Map data) {
     return new Slot(
         _getString(data, "name", ""),
@@ -39,37 +40,37 @@ class _DragonBonesParserJson5 {
         _getInt(data, "displayIndex", 0),
         _getInt(data, "z", 0));
   }
-
+  
   static Skin _parseSkin(Map data) {
     return new Skin(
         _getString(data, "name", ""),
         _getList<SkinSlot>(data, "slot", _parseSkinSlot));
   }
-
+  
   static SkinSlot _parseSkinSlot(Map data) {
     return new SkinSlot(
         _getString(data, "name", ""),
         _getList<Display>(data, "display", _parseDisplay));
   }
-
+  
   static Display _parseDisplay(Map data) {
     var name = _getString(data, "name", "");
     var type = _getString(data, "type", "");
     var transform = _getTransform(data, "transform");
-
+    
     if (type == "mesh") {
       var vertices = _getFloat32List(data, "vertices", (v) => v.toDouble());
       var uvs = _getFloat32List(data, "uvs", (v) => v.toDouble());
       var triangles = _getInt16List(data, "triangles", (v) => v);
       var edges = _getInt16List(data, "edges", (v) => v);
       return new Mesh(name, type, transform, vertices, uvs, triangles, edges);
-    } else {
+      } else {
       return new Display(name, type, transform);
-    }
+      }
   }
-
+  
   //---------------------------------------------------------------------------
-
+  
   static Animation _parseAnimation(Map data) {
     return new Animation(
         _getString(data, "name", ""),
@@ -79,19 +80,19 @@ class _DragonBonesParserJson5 {
         _getList<SlotAnimation>(data, "slot", _parseAnimationSlot),
         _getList<MeshAnimation>(data, "ffd", _parseAnimationMesh));
   }
-
+  
   static BoneAnimation _parseAnimationBone(Map data) {
     return new BoneAnimation(
         _getString(data, "name", ""),
         _getList<BoneAnimationFrame>(data, "frame", _parseAnimationBoneFrame));
   }
-
+  
   static SlotAnimation _parseAnimationSlot(Map data) {
     return new SlotAnimation(
         _getString(data, "name", ""),
         _getList<SlotAnimationFrame>(data, "frame", _parseAnimationSlotFrame));
   }
-
+  
   static MeshAnimation _parseAnimationMesh(Map data) {
     return new MeshAnimation(
         _getString(data, "name", ""),
@@ -99,7 +100,7 @@ class _DragonBonesParserJson5 {
         _getString(data, "skin", ""),
         _getList<MeshAnimationFrame>(data, "frame", _parseAnimationMeshFrame));
   }
-
+  
   static BoneAnimationFrame _parseAnimationBoneFrame(Map data) {
     var duration = _getInt(data, "duration", 0);
     var easing = _getDoubleOrNull(data, "tweenEasing", 10.0);
@@ -107,7 +108,7 @@ class _DragonBonesParserJson5 {
     var curve = _getCurve(data, "curve");
     return new BoneAnimationFrame(duration, curve, easing, transform);
   }
-
+  
   static SlotAnimationFrame _parseAnimationSlotFrame(Map data) {
     var duration = _getInt(data, "duration", 0);
     var easing = _getDoubleOrNull(data, "tweenEasing", 10.0);
@@ -119,7 +120,7 @@ class _DragonBonesParserJson5 {
         duration, curve, easing,
         colorTransform, displayIndex, zOrder);
   }
-
+  
   static MeshAnimationFrame _parseAnimationMeshFrame(Map data) {
     var duration = _getInt(data, "duration", 0);
     var offset = _getInt(data, "offset", 0);
@@ -128,9 +129,9 @@ class _DragonBonesParserJson5 {
     var curve = _getCurve(data, "curve");
     return new MeshAnimationFrame(duration, curve, easing, offset, vertices);
   }
-
+  
   //---------------------------------------------------------------------------
-
+  
   static Transform _getTransform(Map data, String key) {
     var value = data.containsKey(key) ? data[key] : new Map();
     var transform = new Transform();
@@ -142,7 +143,7 @@ class _DragonBonesParserJson5 {
     transform.scaleY = _getDouble(value, "scY", 1.0);
     return transform;
   }
-
+  
   static Curve _getCurve(Map data, String key) {
     var value = data.containsKey(key) ? data[key] : null;
     if (value is List && value.length == 4) {
@@ -151,11 +152,11 @@ class _DragonBonesParserJson5 {
       var x2 = value[2].toDouble();
       var y2 = value[3].toDouble();
       return new Curve(x1, y1, x2, y2);
-    } else {
+      } else {
       return null;
-    }
+      }
   }
-
+  
   static ColorTransform _getColorTransform(Map data, String key) {
     var value = data.containsKey(key) ? data[key] : new Map();
     var colorTransform = new ColorTransform();
@@ -169,51 +170,51 @@ class _DragonBonesParserJson5 {
     colorTransform.blueMultiplier = _getInt(value, "bM", 100) * 0.01;
     return colorTransform;
   }
-
+  
   static List<T> _getList<T>(Map<String, dynamic> data, String key, parser) {
     var value = data.containsKey(key) ? data[key] as List : new List();
     return value.map<T>((result) => parser(result)).toList(growable: false);
   }
-
+  
   static Int16List _getInt16List(Map data, String key, parser) {
     var value = data.containsKey(key) ? data[key] as List : new List();
     var list = value.map<int>((result) => parser(result)).toList(growable: false);
     return new Int16List.fromList(list);
   }
-
+  
   static Float32List _getFloat32List(Map data, String key, parser) {
     var value = data.containsKey(key) ? data[key] as List : new List();
     var list = value.map<double>((result) => parser(result)).toList(growable: false);
     return new Float32List.fromList(list);
   }
-
+  
   //---------------------------------------------------------------------------
-
+  
   static String _getString(Map data, String key, String defaultValue) {
     var value = data.containsKey(key) ? data[key] : defaultValue;
     if (value is String) return value;
     throw new StateError("Invalid type for key '$key'");
   }
-
+  
   static int _getInt(Map data, String key, int defaultValue) {
     var value = data.containsKey(key) ? data[key] : defaultValue;
     if (value is int) return value;
     throw new StateError("Invalid type for key '$key'");
   }
-
+  
   static double _getDoubleOrNull(Map data, String key, double defaultValue) {
     var value = data.containsKey(key) ? data[key] : defaultValue;
     if (value is num) return value.toDouble();
     if (value == null) return null;
     throw new StateError("Invalid type for key '$key'");
   }
-
+  
   static double _getDouble(Map data, String key, double defaultValue) {
     var value = data.containsKey(key) ? data[key] : defaultValue;
     if (value is num) return value.toDouble();
     throw new StateError("Invalid type for key '$key'");
   }
-
+  
   static bool _getBool(Map data, String key, bool defaultValue) {
     var value = data.containsKey(key) ? data[key] : defaultValue;
     if (value is bool) return value;
@@ -221,7 +222,7 @@ class _DragonBonesParserJson5 {
     if (value == 1 || value == "true") return true;
     throw new StateError("Invalid type for key '$key'");
   }
-
-}
+  
+  }
 
 
